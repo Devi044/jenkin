72c5b19e0c1e1d7f62a45c91c728fb103cf84757
Ubuntu <ubuntu@ip-172-31-79-98.ec2.internal>
2025-12-17 09:19:49 +0000

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) { 
-  res.send('this is fifth commit'); 
+  res.send('this is last commit'); 
 });
 
 // listen to port 3000 by default
